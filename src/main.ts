c0e6548c0adef9797225803872df053b72c898f8
lucstu <lstuart1@terpmail.umd.edu>
2021-08-04 13:47:11 -0500

    Added test workflow, test coverage file, and edited main.ts to allow on pushes

--- src/main.ts
+++ src/main.ts
@@ -62,10 +62,10 @@ function createMessage(pytestResult: any): string {
 }
 
 async function run(): Promise<void> {
-  if (github.context.eventName !== 'pull_request') {
-    core.info('Comment only will be created on pull requests!')
-    return
-  }
+  // if (github.context.eventName !== 'pull_request') {
+  //   core.info('Comment only will be created on pull requests!')
+  //   return
+  // }
 
   const githubToken = core.getInput('token')
   const pytestFileName = core.getInput('pytest-coverage')
@@ -77,34 +77,36 @@ async function run(): Promise<void> {
 
   const octokit = github.getOctokit(githubToken)
 
+  octokit.create_commit_comment(context.repo, context.sha, message)
+
   // Now decide if we should issue a new comment or edit an old one
-  const {data: comments} = await octokit.issues.listComments({
-    ...context.repo,
-    issue_number: pullRequestNumber ?? 0
-  })
+  // const {data: comments} = await octokit.issues.listComments({
+  //   ...context.repo,
+  //   issue_number: pullRequestNumber ?? 0
+  // })
 
-  const comment = comments.find((comment: any) => {
-    return (
-      comment.user.login === 'github-actions[bot]' &&
-      comment.body.startsWith(
-        '### :white_check_mark: Result of Pytest Coverage\n'
-      )
-    )
-  })
+  // const comment = comments.find((comment: any) => {
+  //   return (
+  //     comment.user.login === 'github-actions[bot]' &&
+  //     comment.body.startsWith(
+  //       '### :white_check_mark: Result of Pytest Coverage\n'
+  //     )
+  //   )
+  // })
 
-  if (comment) {
-    await octokit.issues.updateComment({
-      ...context.repo,
-      comment_id: comment.id,
-      body: message
-    })
-  } else {
-    await octokit.issues.createComment({
-      ...context.repo,
-      issue_number: pullRequestNumber ?? 0,
-      body: message
-    })
-  }
+  // if (comment) {
+  //   await octokit.issues.updateComment({
+  //     ...context.repo,
+  //     comment_id: comment.id,
+  //     body: message
+  //   })
+  // } else {
+  //   await octokit.issues.createComment({
+  //     ...context.repo,
+  //     issue_number: pullRequestNumber ?? 0,
+  //     body: message
+  //   })
+  // }
 }
 
 // eslint-disable-next-line github/no-then
