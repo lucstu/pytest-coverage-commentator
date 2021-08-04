5de46ef05f3d24857e79c64f667d4695b63be992
lucstu <lstuart1@terpmail.umd.edu>
2021-08-04 13:51:55 -0500

    Edited index.js

--- dist/index.js
+++ dist/index.js
@@ -75,28 +75,16 @@ function createMessage(pytestResult) {
 function run() {
     var _a;
     return __awaiter(this, void 0, void 0, function* () {
-        if (github.context.eventName !== 'pull_request') {
-            core.info('Comment only will be created on pull requests!');
-            return;
-        }
-        const githubToken = core.getInput('token');
-        const pytestFileName = core.getInput('pytest-coverage');
-        const message = createMessage(pytestFileName);
-        const context = github.context;
-        const pullRequestNumber = (_a = context.payload.pull_request) === null || _a === void 0 ? void 0 : _a.number;
-        const octokit = github.getOctokit(githubToken);
-        // Now decide if we should issue a new comment or edit an old one
-        const { data: comments } = yield octokit.issues.listComments(Object.assign(Object.assign({}, context.repo), { issue_number: pullRequestNumber !== null && pullRequestNumber !== void 0 ? pullRequestNumber : 0 }));
-        const comment = comments.find((comment) => {
-            return (comment.user.login === 'github-actions[bot]' &&
-                comment.body.startsWith('### :white_check_mark: Result of Pytest Coverage\n'));
-        });
-        if (comment) {
-            yield octokit.issues.updateComment(Object.assign(Object.assign({}, context.repo), { comment_id: comment.id, body: message }));
-        }
-        else {
-            yield octokit.issues.createComment(Object.assign(Object.assign({}, context.repo), { issue_number: pullRequestNumber !== null && pullRequestNumber !== void 0 ? pullRequestNumber : 0, body: message }));
-        }
+      const githubToken = core.getInput('token')
+    const pytestFileName = core.getInput('pytest-coverage')
+
+    const message = createMessage(pytestFileName)
+
+    const context = github.context
+
+    const octokit = github.getOctokit(githubToken)
+
+    octokit.create_commit_comment(context.repo, context.sha, message)
     });
 }
 // eslint-disable-next-line github/no-then
